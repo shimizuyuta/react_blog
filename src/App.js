2f19e563ed0dd4332058bd96d39cb429b8e95e01
clean_water <shmizuyuta213@icloud.com>
2021-10-11 16:21:17 +0900

    first init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   
 
 
   return (
-    <div>
+    <div className='App'>
       <Navbar/>
-            
+      <div className="content">
+        <Home/>
+      </div>
     </div>
   );
 }
+
 
 export default App;
