fbf09540d8837488ae954ad50c06d8bdce6d82aa
clean_water <shmizuyuta213@icloud.com>
2021-10-11 20:28:43 +0900

    外部データ読み込み時にloading追加・読み込み中エラーが出た時にエラー表示

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,17 +4,28 @@ import BlogList from '../BlogList/BlogList'
 const Home = () => {
    
     const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] =useState(true);
+    const [error,setError] = useState(null);
 
     const url = 'http://localhost:8000/blogs'
     
     useEffect(()=>{
         fetch(url)
         .then(res =>{
+            if(!res.ok){
+                throw Error('could not fetch data');
+            }
             return res.json()
         })
         .then((data)=>{
             console.log(data,'data')
             setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch((e)=>{
+            setIsPending(false);
+            setError(e.message)
         })
     },[])
 
@@ -23,6 +34,8 @@ const Home = () => {
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs &&<BlogList blogs={blogs} title='all blog' />}
         </div>
  
