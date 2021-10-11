7bae390c20a7a28f9860d4b9a46526e02490bd6f
clean_water <shmizuyuta213@icloud.com>
2021-10-11 20:48:57 +0900

    fetchコンポーネントに切り分け

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,9 @@
-import React,{ useState,useEffect } from 'react'
+import { useState,useEffect } from 'react'
+import useFetch from '../../UseFetch/useFetch'
 import BlogList from '../BlogList/BlogList'
 
 const Home = () => {
-   
-    const [blogs,setBlogs] = useState(null);
-    const [isPending,setIsPending] =useState(true);
-    const [error,setError] = useState(null);
-
-    const url = 'http://localhost:8000/blogs'
-    
-    useEffect(()=>{
-        fetch(url)
-        .then(res =>{
-            if(!res.ok){
-                throw Error('could not fetch data');
-            }
-            return res.json()
-        })
-        .then((data)=>{
-            console.log(data,'data')
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch((e)=>{
-            setIsPending(false);
-            setError(e.message)
-        })
-    },[])
-
-
+    const{data:blogs,isPending,error} = useFetch('http://localhost:8000/blogs')
 
 
     return (
