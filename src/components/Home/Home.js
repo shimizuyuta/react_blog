c7f08e680e15f3e71cccaa175f5d070d6ceec2e4
clean_water <shmizuyuta213@icloud.com>
2021-10-11 20:15:28 +0900

    useEffect追加・元のサンプルデータ移行

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,29 @@
-import React,{ useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import BlogList from '../BlogList/BlogList'
 
 const Home = () => {
    
-    const [blogs,setBlogs] = useState([
-        {title:'hello world1',body:'lorem...........',author:'mario',id:1},
-        {title:'hello world2',body:'lorem...........',author:'田中',id:2},
-        {title:'hello world3',body:'lorem...........',author:'佐藤',id:3}
-    ])
-    const handleDelete = (id) =>{
-        const newBlogs =blogs.filter(blog=> blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs,setBlogs] = useState(null);
+
+    const url = 'http://localhost:8000/blogs'
+    
+    useEffect(()=>{
+        fetch(url)
+        .then(res =>{
+            return res.json()
+        })
+        .then((data)=>{
+            console.log(data,'data')
+            setBlogs(data);
+        })
+    },[])
 
 
 
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='all blog' handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author==='佐藤')} title='佐藤 blog'/>
+            {blogs &&<BlogList blogs={blogs} title='all blog' />}
         </div>
  
     )
